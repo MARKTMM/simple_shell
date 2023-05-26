5af29347a39f744e0e3193476d118de92388e854
M@rk <115602321+MARKTMM@users.noreply.github.com>
2023-05-26 22:38:41 +0300

    Update shll.h

--- shll.h
+++ shll.h
@@ -14,17 +14,17 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
-/* Global environemnt */
+/* environemnt Global */
 extern char **environ;
-/* Global program name */
+/* program Global name */
 char *name;
-/* Global history counter */
+/* history Global counter */
 int hist;
 
 /**
- * struct list_s - A new struct type defining a linked list.
- * @dir: A directory path.
- * @next: A pointer to another struct list_s.
+ * struct list_s - new struct type defining linked list
+ * @dir: directory
+ * @next: pointer to another struct list_s.
  */
 typedef struct list_s
 {
@@ -33,9 +33,9 @@ typedef struct list_s
 } list_t;
 
 /**
- * struct builtin_s - A new struct type defining builtin commands.
- * @name: The name of the builtin command.
- * @f: A function pointer to the builtin command's function.
+ * struct builtin_s - struct type defining builtin commands
+ * @name: name of the builtin command
+ * @f: function pointer to builtin command's function
  */
 typedef struct builtin_s
 {
@@ -44,10 +44,10 @@ typedef struct builtin_s
 } builtin_t;
 
 /**
- * struct alias_s - A new struct defining aliases.
- * @name: The name of the alias.
- * @value: The value of the alias.
- * @next: A pointer to another struct alias_s.
+ * struct alias_s - new struct defining aliases
+ * @name: name of the alias
+ * @value: value of alias
+ * @next: pointer to another struct alias_s
  */
 typedef struct alias_s
 {
@@ -56,10 +56,10 @@ typedef struct alias_s
 	struct alias_s *next;
 } alias_t;
 
-/* Global aliases linked list */
+/* aliases Global linked list */
 alias_t *aliases;
 
-/* Main Helpers */
+/* Helpers Main */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_strtok(char *line, char *delim);
@@ -69,7 +69,7 @@ int execute(char **args, char **front);
 void free_list(list_t *head);
 char *_itoa(int num);
 
-/* Input Helpers */
+/* Helpers Input */
 void handle_line(char **line, ssize_t read);
 void variable_replacement(char **args, int *exe_ret);
 char *get_args(char *line, int *exe_ret);
@@ -80,7 +80,7 @@ int check_args(char **args);
 void free_args(char **args, char **front);
 char **replace_aliases(char **args);
 
-/* String functions */
+/* functions String */
 int _strlen(const char *s);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
@@ -90,7 +90,7 @@ int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
-/* Builtins */
+/* Builtin */
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
 int shellby_env(char **args, char __attribute__((__unused__)) **front);
@@ -100,12 +100,12 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front);
 int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
-/* Builtin Helpers */
+/* Helpers Builtin */
 char **_copyenv(void);
 void free_env(void);
 char **_getenv(const char *var);
 
-/* Error Handling */
+/* Handling Error */
 int create_error(char **args, int err);
 char *error_env(char **args);
 char *error_1(char **args);
@@ -115,7 +115,7 @@ char *error_2_syntax(char **args);
 char *error_126(char **args);
 char *error_127(char **args);
 
-/* Linkedlist Helpers */
+/* Helpers Linkedlist */
 alias_t *add_alias_end(alias_t **head, char *name, char *value);
 void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
