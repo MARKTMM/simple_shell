02149136ba7cc9f206bf63f070aea1e812520032
M@rk <115602321+MARKTMM@users.noreply.github.com>
2023-05-26 22:43:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ void sig_handler(int sig);
 int execute(char **args, char **front);
 
 /**
- * sig_handler - Prints a new prompt upon a signal.
- * @sig: The signal.
+ * sig_handler - Prints new prompt upon signal
+ * @sig: signal
  */
 void sig_handler(int sig)
 {
@@ -17,12 +17,12 @@ void sig_handler(int sig)
 }
 
 /**
- * execute - Executes a command in a child process.
- * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
+ * execute - Executes command in child process
+ * @args: array of arguments
+ * @front: double pointer to beginning of args
  *
- * Return: If an error occurs - a corresponding error code.
- *         O/w - The exit value of the last executed command.
+ * Return: If error occurs -corresponding error code
+ *         Otherwise - exit value last executed command
  */
 int execute(char **args, char **front)
 {
@@ -75,11 +75,11 @@ int execute(char **args, char **front)
 }
 
 /**
- * main - Runs a simple UNIX command interpreter.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - simple UNIX interpreter.
+ * @argc: number of arguments supplied
+ * @argv: array of pointers to the arguments
  *
- * Return: The return value of the last executed command.
+ * Return: return value of last command
  */
 int main(int argc, char *argv[])
 {
